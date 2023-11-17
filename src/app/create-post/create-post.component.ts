73e469a287bad4836bc1fece27179cd80afda828
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2023-11-17 11:14:24 +0100

    apply a form Driven into Create Post

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -2,6 +2,7 @@ import { PostService } from './../services/post.service';
 import { Component, inject } from '@angular/core';
 import { Post } from '../models/post';
 import { Router } from '@angular/router'
+import { FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
@@ -18,12 +19,23 @@ export class CreatePostComponent {
     body: ''
   }
 
-  createPost() {
-    this.postService.persistPost(this.myPost).subscribe({
+  createPost(form: NgForm) {
+
+    if(form.invalid) {
+      alert('sir tan3ass')
+      return;
+    }
+    
+    this.postService.persistPost(form.value).subscribe({
       next: res => {
         this.router.navigateByUrl('/blog')
       },
       error: err => console.log(err)
     })
   }
+
+  info(data: any) {
+    console.log(data)
+  }
+
 }
